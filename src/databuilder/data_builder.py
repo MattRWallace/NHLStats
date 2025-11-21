e3d4dd82fbd2e2f0e62805d3e157b0227ea6ee3e
Matt Wallace <matthew.r.wallace@live.com>
2025-11-21 14:21:33 -0600

    Move summary logic to dependancy injection

--- src/databuilder/data_builder.py
+++ src/databuilder/data_builder.py
@@ -6,6 +6,7 @@ from nhlpy import NHLClient
 
 from model.game_entry import GameEntry
 from model.game_type import GameType
+from model.naive_player_summarizer import NaivePlayerSummarizer
 from model.player_info import GoalieInfo, SkaterInfo
 from model.seasons import PastSeasons
 from model.team_map import TeamMap
@@ -20,7 +21,12 @@ class DataBuilder:
     _client = NHLClient()
 
     @staticmethod
-    def build_games():
+    def build():
+        testSummarizer = NaivePlayerSummarizer()
+        DataBuilder.build_games(testSummarizer)
+
+    @staticmethod
+    def build_games(summarizer):
         
         """
         Avoid multiple rows for a single game by recoding the IDs for games already processed.
@@ -49,17 +55,13 @@ class DataBuilder:
                                 logger.info(f"Skipping game '{game["id"]}' which is not a regular season game.")
                                 continue
                             
-                            # Get roster scores
-                            homeRoster = DataBuilder.build_players(
-                                box_score["playerByGameStats"]["homeTeam"]
-                                )
-                            
-                            awayRoster = DataBuilder.build_players(
+                            summary = summarizer.summarize(
+                                box_score["playerByGameStats"]["homeTeam"],
                                 box_score["playerByGameStats"]["awayTeam"]
-                                )
+                            )
                             
                             entry = GameEntry.from_json(box_score)
-                            entry.add_roster(homeRoster, awayRoster)
+                            entry.add_roster(*summary)
                             
                             logger.info(f"Adding game entry to data set: '{entry}'.")
                             data.append(repr(entry).split(','))
@@ -71,140 +73,8 @@ class DataBuilder:
                 except Exception as e:
                     print("\033[31mException occured. Check logs.\033[0m")
                     logger.exception(f"Exception processing team_season_schedule query. Exception: '{str(e)}', games: '{json.dumps(games, indent=4)}', box_score: '{json.dumps(box_score, indent=4)}'.", stack_info=True)
-
+                
             df = pd.DataFrame(data, columns=GameEntry.get_headers())
             df.to_csv(f"{season}.csv", index=False)
                                 
         logger.info("Completed data fetch")
-
-    @staticmethod
-    def build_players(roster):
-        forwards = DataBuilder.summarize_skaters(roster["forwards"])
-        defense = DataBuilder.summarize_skaters(roster["defense"])
-        goalies = DataBuilder.summarize_goalies(roster["goalies"])
-        return f"{forwards},{defense},{goalies}"
-        
-    @staticmethod
-    def summarize_skaters(players):
-        player_objects = []
-        for player in players:
-            player_objects.append(SkaterInfo.from_json(player))
-        
-        count = 0   
-        goals = 0
-        assists = 0
-        points = 0
-        plus_minus = 0
-        pim = 0
-        hits = 0
-        pp_goals = 0
-        sog = 0
-        faceoff_win_pct = 0
-        toi = 0
-        blocked_shots = 0
-        giveaways = 0
-        takeaways = 0
-        
-        for player in player_objects:
-            count += 1
-            goals += player.goals
-            assists += player.assists
-            points += player.points
-            plus_minus += player.plus_minus
-            pim += player.pim
-            hits += player.hits
-            pp_goals += player.pp_goals
-            sog += player.sog
-            faceoff_win_pct += player.faceoff_win_pct
-            toi += player.toi
-            blocked_shots += player.blocked_shots
-            giveaways += player.giveaways
-            takeaways += player.takeaways
-        
-        # TODO: can't just average the FO %, it needs to be weighted    
-        summary = SkaterInfo(
-            goals,
-            assists,
-            points,
-            plus_minus,
-            pim,
-            hits,
-            pp_goals,
-            sog,
-            faceoff_win_pct,
-            toi,
-            blocked_shots,
-            giveaways,
-            takeaways
-        )
-        return repr(summary)
-    
-    @staticmethod
-    def summarize_goalies(players):
-        player_objects = []
-        for player in players:
-            player_objects.append(GoalieInfo.from_json(player))
-
-        count = 0
-        es_shots_against = 0
-        es_saves = 0
-        pp_shots_against = 0
-        pp_saves = 0
-        sh_shots_against = 0
-        sh_saves = 0
-        save_shots_against = 0
-        save_pct = 0
-        es_goals_against = 0
-        pp_goals_against = 0
-        sh_goals_against = 0
-        pim = 0
-        goals_against = 0
-        toi = 0
-        # starter = 0
-        # decision = 0
-        shots_against = 0
-        saves = 0
-        
-        for player in player_objects:
-            count += 1
-            es_shots_against += player.es_shots_against
-            es_saves += player.es_saves
-            pp_shots_against += player.pp_shots_against
-            pp_saves += player.pp_saves
-            sh_shots_against += player.sh_shots_against
-            sh_saves += player.sh_saves
-            save_shots_against += player.save_shots_against
-            save_pct += player.save_pct
-            es_goals_against += player.es_goals_against
-            pp_goals_against += player.pp_goals_against
-            sh_goals_against += player.sh_goals_against
-            pim += player.pim
-            goals_against += player.goals_against
-            toi += player.toi
-            # starter += player.starter
-            # decision += player.decision
-            shots_against += player.shots_against
-            saves += player.saves
-            
-        # TODO: can't just average the FO %, it needs to be weighted
-        summary = GoalieInfo(
-            es_shots_against,
-            es_saves,
-            pp_shots_against,
-            pp_saves,
-            sh_shots_against,
-            sh_saves,
-            save_shots_against,
-            save_pct,
-            es_goals_against,
-            pp_goals_against,
-            sh_goals_against,
-            pim,
-            goals_against,
-            toi,
-            # starter,
-            # decision,
-            shots_against,
-            saves
-        )
-        return repr(summary)
---
+++ src/model/naive_player_summarizer.py
@@ -0,0 +1,139 @@
+from model.player_info import GoalieInfo, SkaterInfo
+
+
+class NaivePlayerSummarizer:
+
+    def summarize(self, homeRoster, awayRoster):
+        homeSummary = self.summarize_roster(homeRoster)
+        awaySummary = self.summarize_roster(awayRoster)
+
+        return homeSummary, awaySummary
+    
+    def summarize_roster(self, roster):
+        forwards = self.summarize_skaters(roster["forwards"])
+        defense = self.summarize_skaters(roster["defense"])
+        goalies = self.summarize_goalies(roster["goalies"])
+        return f"{forwards},{defense},{goalies}"
+        
+    def summarize_skaters(self, players):
+        player_objects = []
+        for player in players:
+            player_objects.append(SkaterInfo.from_json(player))
+        
+        count = 0   
+        goals = 0
+        assists = 0
+        points = 0
+        plus_minus = 0
+        pim = 0
+        hits = 0
+        pp_goals = 0
+        sog = 0
+        faceoff_win_pct = 0
+        toi = 0
+        blocked_shots = 0
+        giveaways = 0
+        takeaways = 0
+        
+        for player in player_objects:
+            count += 1
+            goals += player.goals
+            assists += player.assists
+            points += player.points
+            plus_minus += player.plus_minus
+            pim += player.pim
+            hits += player.hits
+            pp_goals += player.pp_goals
+            sog += player.sog
+            faceoff_win_pct += player.faceoff_win_pct
+            toi += player.toi
+            blocked_shots += player.blocked_shots
+            giveaways += player.giveaways
+            takeaways += player.takeaways
+        
+        # TODO: can't just average the FO %, it needs to be weighted    
+        summary = SkaterInfo(
+            goals,
+            assists,
+            points,
+            plus_minus,
+            pim,
+            hits,
+            pp_goals,
+            sog,
+            faceoff_win_pct,
+            toi,
+            blocked_shots,
+            giveaways,
+            takeaways
+        )
+        return repr(summary)
+    
+    def summarize_goalies(self, players):
+        player_objects = []
+        for player in players:
+            player_objects.append(GoalieInfo.from_json(player))
+
+        count = 0
+        es_shots_against = 0
+        es_saves = 0
+        pp_shots_against = 0
+        pp_saves = 0
+        sh_shots_against = 0
+        sh_saves = 0
+        save_shots_against = 0
+        save_pct = 0
+        es_goals_against = 0
+        pp_goals_against = 0
+        sh_goals_against = 0
+        pim = 0
+        goals_against = 0
+        toi = 0
+        # starter = 0
+        # decision = 0
+        shots_against = 0
+        saves = 0
+        
+        for player in player_objects:
+            count += 1
+            es_shots_against += player.es_shots_against
+            es_saves += player.es_saves
+            pp_shots_against += player.pp_shots_against
+            pp_saves += player.pp_saves
+            sh_shots_against += player.sh_shots_against
+            sh_saves += player.sh_saves
+            save_shots_against += player.save_shots_against
+            save_pct += player.save_pct
+            es_goals_against += player.es_goals_against
+            pp_goals_against += player.pp_goals_against
+            sh_goals_against += player.sh_goals_against
+            pim += player.pim
+            goals_against += player.goals_against
+            toi += player.toi
+            # starter += player.starter
+            # decision += player.decision
+            shots_against += player.shots_against
+            saves += player.saves
+            
+        # TODO: can't just average the FO %, it needs to be weighted
+        summary = GoalieInfo(
+            es_shots_against,
+            es_saves,
+            pp_shots_against,
+            pp_saves,
+            sh_shots_against,
+            sh_saves,
+            save_shots_against,
+            save_pct,
+            es_goals_against,
+            pp_goals_against,
+            sh_goals_against,
+            pim,
+            goals_against,
+            toi,
+            # starter,
+            # decision,
+            shots_against,
+            saves
+        )
+        return repr(summary)
